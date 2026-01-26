715339d237072fbb5b52e758cd5389e169134e81
Ryu <taiwailanng@gmail.com>
2026-01-26 18:40:06 +0530

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -126,7 +126,7 @@ function GUI.Init(vars)
             startPos = GUI.MainFrame.Position
             
             input.Changed:Connect(function()
-                if input.UserInputState == Enum.UserInputState.End then debugging = false dragging = false end
+                if input.UserInputState == Enum.UserInputState.End then  end
             end)
         end
     end)
